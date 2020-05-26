1b630bdd3536f1d08c6cf5b6ad91298777d4ecfd
William White <william.white@nthrive.com>
2020-05-26 17:25:04 -0600

    change login file name

--- register.php
+++ register.php
@@ -78,7 +78,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
-                header("location: login.php");
+                header("location: index.php");
             } else{
                 echo "Something went wrong. Please try again later.";
             }
@@ -130,7 +130,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <input type="submit" class="btn btn-primary" value="Submit">
                             <input type="reset" class="btn btn-default" value="Reset">
                         </div>
-                        <p>Already have an account? <br><a href="login.php">Login here</a></p>
+                        <p>Already have an account? <br><a href="index.php">Login here</a></p>
                     </form>
                 <!-- </div>
             </div>
